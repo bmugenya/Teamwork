413af5bd05c64413666baa7773a034621856cc45
bmugenya <bmugenya26@gmail.com>
2019-10-29 23:56:57 +0300

    separated function from routes

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { pool } = require('./db_con')
 
 const app = express()
+const model = require('./model/model')
 
 app.use(bodyParser.json())
-
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const getAdmin = (request, response) => {
-  pool.query('SELECT * FROM Admin', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-
-const addAdmin = (request, response) => {
-  const { email, password } = request.body
-
-  pool.query('INSERT INTO Admin (email, password) VALUES ($1, $2)', [email, password], error => {
-    if (error) {
-      throw error
-    }
-    response.status(201).json({ status: 'success', message: 'Admin added.' })
-  })
-}
-
-app.get('/admin/getadmin',getAdmin)
-app.post('/admin/addadmin', addAdmin)
+app.get('/admin/getadmin',model.getAdmin)
+app.post('/admin/addadmin', model.addAdmin)
 
 
 module.exports = app;
