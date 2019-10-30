c28f71ea60e20dc497b968de555d7fba6ad83753
bmugenya <bmugenya26@gmail.com>
2019-10-30 22:37:27 +0300

    [FEATURE #169397927] imported bcrypt

--- backend/model/model.js
+++ backend/model/model.js
@@ -1,5 +1,7 @@
-const { pool } = require('../db_con')
+const { pool } = require('../db_con');
+const bcrypt = require('bcrypt');
 
+const saltRounds = 10;
 const getAdmin = (request, response) => {
   pool.query('SELECT * FROM Admin', (error, results) => {
     if (error) {
@@ -13,9 +15,14 @@ const getAdmin = (request, response) => {
 
 
 const addAdmin = (request, response) => {
+
+
   const { email, password } = request.body
 
-  pool.query('INSERT INTO Admin (email, password) VALUES ($1, $2)', [email, password], error => {
+
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+
+  pool.query('INSERT INTO Admin (email, password) VALUES ($1, $2)', [email, hash], error => {
     if (error) {
          response.status(400).json({
          error:error
@@ -23,10 +30,32 @@ const addAdmin = (request, response) => {
     }
     response.status(201).json({ status: 'success', message: 'Admin added.' })
   })
+
+
+});
+
+
+
 }
+
+
+const login = (request, response) => {
+  pool.query('SELECT * FROM Admin', (error, results) => {
+    if (error) {
+      response.status(400).json({
+          error:error
+      });
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+
+
 
 
 module.exports = {
     getAdmin,
     addAdmin,
+    login,
 }
