af89580c1da65d774940091b912a2e6f104fc4c6
bmugenya <bmugenya26@gmail.com>
2019-11-05 16:07:14 +0300

    [FEATURE #169397927] added JWT

--- backend/model/model.js
+++ backend/model/model.js
@@ -1,6 +1,7 @@
-const { pool } = require('../db_con')
+ const { pool } = require('../db_con')
 const bcrypt = require('bcrypt')
-const passport = require('passport')
+const jwt = require('jsonwebtoken')
+
 
 const saltRounds = 10;
 const getAdmin = (request, response) => {
@@ -10,13 +11,10 @@ const getAdmin = (request, response) => {
           error:error
       });
     }
-    console.log(request.user)
-    console.log(request.isAuthenticated())
 
     response.status(200).json(results.rows)
   })
 }
-
 
 const addAdmin = (request, response) => {
   const { email, password } = request.body
@@ -28,40 +26,60 @@ const addAdmin = (request, response) => {
          error:error
       });
     }
-    pool.query("select currval(pg_get_serial_sequence('admin','id')) as user_id",function(error,results,fields){
-
-      if(error) throw error;
-
-      const user_id = results.rows[0].user_id
-      request.login(user_id, function(err){
-      response.status(201).json({ status: 'success', message: 'Admin added.' })
-      })
+   response.status(201).json({ status: 'success', message: 'Admin added.' })
 
     })
 
   })
-
-});
 }
 
 
 
-const login = (request, response) => {
-   const { username, password } = request.body
-   response.json(username,password)
+const login = (request, res, next) => {
+    const { username, password } = request.body
+   pool.query('select id, password from admin where email = $1',[username],function(error,results,fields) {
 
+
+    if(results.rows.length === 0){
+      console.log('invalid')
+    } else{
+     const hash = results.rows[0].password.toString()
+
+     bcrypt.compare(password, hash ,function(error,response){
+       if(!response){
+         return res.status(401).json({
+           error: 'Invalid password'
+         })
+       }
+            const token = jwt.sign(
+        { user_id:results.rows[0].id},
+        'RANDOM_TOKEN_SECRET',{expiresIn:'24h'});
+
+       res.status(200).json({
+         user_id:results.rows[0].id,
+         token:token
+       })
+
+   }
+ )}
+})
+ }
+
+
+const logout = (request, response,next) => {
+    request.session.destroy((err) => {
+      if(err) return next(err)
+
+      request.logout()
+      response.sendStatus(200)
+    })
 }
 
-passport.serializeUser(function(user_id,done){
-  done(null, user_id)
-})
 
-passport.deserializeUser(function(user_id,done){
-  done(null, user_id)
-})
 
 module.exports = {
     getAdmin,
     addAdmin,
     login,
+    logout,
 }
