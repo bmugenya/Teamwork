9a41cbdf0b4683ab9155881b769c3a3548cec22d
bmugenya <bmugenya26@gmail.com>
2019-11-10 11:50:16 +0300

    [FEATURE #169664363] added imports from cloudinary

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,24 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const path = require('path');
+
+const config = require('./config/cloudinaryConfig')
+
 
 const app = express()
 const model = require('./model/model')
 const auth = require('./middleware/auth')
+const multer = require('./middleware/multer')
 
+const uploader = config.uploader
+const cloudinaryConfig = config.cloudinaryConfig
+const multerUploads = multer.multerUploads
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.resolve(__dirname, 'src/public')));
+app.use('*', cloudinaryConfig);
 
 
 app.post('/api/v1/logout',model.logout)
@@ -20,8 +30,9 @@ app.post('/api/v1/auth/create-user',auth, model.addEmployee)
 app.post('/api/v1/auth/admin/signin', model.adminLogin)
 app.get('/api/v1/getadmin',auth, model.getAdmin)
 
-//USER
+//EMPLOYEE
 app.post('/api/v1/auth/signin', model.employeeLogin)
 app.post('/api/v1/articles',auth, model.newArticle)
+app.post('/api/v1/gifs',auth,multerUploads, model.newGif)
 
 module.exports = app;
